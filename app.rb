37bcd6ebdd538310a7871ec04d7aab735a28cc57
Hans Foster <fosterhans@gmail.com>
2019-12-16 03:27:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,7 +5,19 @@ class App < Sinatra::Base
   
   get '/reversename:name' do 
     @name = @params[:name]
-    @name.reverse 
+    "#{@name.reverse}" 
   end
+  
+  get '/square/:number' do
+    @num = @params[:number].to_i 
+    "#{@num ^ 2}"
+  end
+  
+  get '/say/:number/:phrase' do
+    @phrase = @params[:phrase]
+    @num = @params[:number].to_i 
+    temp_string = ""
+    @num.times do 
+      temp_string +=
 
 end
