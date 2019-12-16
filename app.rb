94ef5dd72ef8cea69af5b8f67c61512ed09020c5
Hans Foster <fosterhans@gmail.com>
2019-12-16 03:45:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,22 +23,22 @@ class App < Sinatra::Base
   #   "#{@temp_string}"
   # end
   
-  # get 'say/:word1/:word2/:word3/:word4/:word5' do 
-  #   @word1 = @params[:word1]
-  #   @word2 = @params[:word2]
-  #   @word3 = @params[:word3]
-  #   @word4 = @params[:word4]
-  #   @word5 = @params[:word5]
+  get 'say/:word1/:word2/:word3/:word4/:word5' do 
+    @word1 = @params[:word1]
+    @word2 = @params[:word2]
+    @word3 = @params[:word3]
+    @word4 = @params[:word4]
+    @word5 = @params[:word5]
     
-  #   "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
-  # end
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
+  end
   
-  # get '/:operation/:number1/:number2'
-  #   @operation = @params[:operation]
-  #   @num1 = @params[:number1].to_i 
-  #   @num2 = @params[:number2].to_i 
-  #   @solution = @num1 + @num2 
-  #   "#{@solution.to_s}"
-  # end
+  get '/:operation/:number1/:number2'
+    @operation = @params[:operation]
+    @num1 = @params[:number1].to_i 
+    @num2 = @params[:number2].to_i 
+    @solution = @num1 + @num2 
+    "#{@solution.to_s}"
+  end
 
 end
